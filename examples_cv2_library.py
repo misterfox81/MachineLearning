4ce7c786070e244346f5a062e65956b4dac702ad
root <lspezia81@gmail.com>
2019-06-13 18:29:07 +0200

    Prima modifica codice

--- examples_cv2_library.py
+++ examples_cv2_library.py
@@ -5,9 +5,11 @@ import cv2
 # load the input image and show its dimensions, keeping in mind that
 # images are represented as a multi-dimensional NumPy array with
 # shape no. rows (height) x no. columns (width) x no. channels (depth)
+# Carica immagine nella forma di array
 image = cv2.imread("img/1.jpeg")
 (h, w, d) = image.shape
 print("width={}, height={}, depth={}".format(w, h, d))
+
 
 # access the RGB pixel located at x=50, y=100, keepind in mind that
 # OpenCV stores images in BGR order rather than RGB
